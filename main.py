b0ffbab31cfc322eef888f0155027a08d9550ab2
Jarno Bucher <jarno.bucher@bluewin.ch>
2025-08-08 12:10:02 +0200

    transfrom fix maybe

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ class VideoCamera:
             config = self.camera.create_video_configuration(
                 main={"size": (640, 480), "format": "RGB888"},
                 lores={"size": (320, 240), "format": "YUV420"},
-                transform={"hflip": True, "vflip": True}  # 180-degree rotation
+                transform={"hflip": True}  # 180-degree rotation
             )
             self.camera.configure(config)
             
